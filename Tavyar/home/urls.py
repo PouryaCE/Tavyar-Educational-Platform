b5b8ce3fb62df71d12e71fff3c6d0bcdb84865f1
PouryaCE <pouryajanparvar@yahoo.com>
2025-12-26 17:50:32 +0330

    help page url

--- Tavyar/home/urls.py
+++ Tavyar/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index_view, rules_view
+from .views import index_view, rules_view, help_view
 app_name = 'home'
 
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('rules', rules_view, name='rules'),
+    path('help', help_view, name='help'),
 ]
