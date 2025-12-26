1d0b1dd307037f32e7f16d96f8ad8a5c431853d2
PouryaCE <pouryajanparvar@yahoo.com>
2025-12-26 17:48:29 +0330

    some features updated

--- Tavyar/home/urls.py
+++ Tavyar/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, rules_view
 app_name = 'home'
 
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('rules', rules_view, name='rules'),
 ]
--- Tavyar/home/views.py
+++ Tavyar/home/views.py
@@ -8,3 +8,7 @@ def index_view(request):
     projects = Project.objects.order_by('-created_at')[:3]
     context = {'projects': projects}
     return render(request, template_name="home/index.html", context=context)
+
+
+def rules_view(request):
+    return render(request, template_name="home/rules.html")
